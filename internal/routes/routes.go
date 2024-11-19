d0f10c9778cd3f677d7e618894a5df0ac7f7d0c3
abdulazizOmonov <abdulaziomonov4@gmail.com>
2024-11-19 09:50:43 +0500

    common_routes_set_up

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,5 +3,12 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func SetupRoutes(r *gin.Engine) {
-
+	connection := r.Group("/connection")
+	connection.POST("/create")
+	connection.POST("/close")
+	fileFolder := r.Group("/file-foleder")
+	fileFolder.GET("/path")
+	fileFolder.POST("/create")
+	fileFolder.PUT("/edit")
+	fileFolder.DELETE("/delete")
 }
